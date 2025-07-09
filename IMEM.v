798c83a86e46fa7afe3a4ddc0bbed600c7597403
tiep3607 <nvtiep2002@gmail.com>
2025-07-09 22:58:40 +0700

    Update code

--- IMEM.v
+++ IMEM.v
@@ -6,6 +6,15 @@ module IMEM (
     
     assign inst = memory[addr[31:2]]; // Word-aligned address
 
+    initial begin
+        if ($test$plusargs("NOIMEM") == 0) begin
+            $display("IMEM: Loading instructions from mem/imem.hex");
+            $readmemh("mem/imem.hex", memory);
+        end else begin
+            $display("IMEM: Skipping instruction memory load (NOIMEM set)");
+        end
+    end
+
     always @(*) begin
         $display("IMEM: Cycle=%0d, addr=%h, inst=%h", $time/10, addr, inst);
     end
